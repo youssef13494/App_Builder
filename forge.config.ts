60b403e9047cb36743ceb9360b1802d1edb6f6e8
Will Chen <willchen90@gmail.com>
2025-04-29 22:00:48 -0700

    windows code-sign (follow electron-fiddle)

--- forge.config.ts
+++ forge.config.ts
@@ -68,7 +68,9 @@ const config: ForgeConfig = {
   },
   makers: [
     new MakerSquirrel({
-      signWithParams: `/csp "DigiCert Signing Manager KSP" /kc ${process.env.DIGICERT_KEYPAIR_ALIAS} /f ${process.env.SM_CLIENT_CERT_FILE} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`,
+      signWithParams: `/sha1 ${process.env.SM_CODE_SIGNING_CERT_SHA1_HASH} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`,
+
+      // signWithParams: `/csp "DigiCert Signing Manager KSP" /kc ${process.env.DIGICERT_KEYPAIR_ALIAS} /f ${process.env.SM_CLIENT_CERT_FILE} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`,
       // windowsSign: {
 
       // certificateFile: process.env.SM_CLIENT_CERT_FILE,
