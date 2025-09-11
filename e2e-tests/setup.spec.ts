2e53014f290f96f7cdb3e3571da78bdb0ab4f88a
Will Chen <willchen90@gmail.com>
2025-09-11 11:13:33 -0700

    Remove URL assertions for setup.spec.ts (fails on windows) (#1258)

--- e2e-tests/setup.spec.ts
+++ e2e-tests/setup.spec.ts
@@ -12,7 +12,6 @@ testSetup("setup ai provider", async ({ po }) => {
   await expect(
     po.page.getByRole("heading", { name: "Configure Google" }),
   ).toBeVisible();
-  expect(po.page.url()).toEqual("file:///providers/google");
 
   await po.page.getByRole("button", { name: "Go Back" }).click();
   await po.page
@@ -21,11 +20,9 @@ testSetup("setup ai provider", async ({ po }) => {
   await expect(
     po.page.getByRole("heading", { name: "Configure OpenRouter" }),
   ).toBeVisible();
-  expect(po.page.url()).toEqual("file:///providers/openrouter");
 
   await po.page.getByRole("button", { name: "Go Back" }).click();
   await po.page
     .getByRole("button", { name: "Setup other AI providers" })
     .click();
-  expect(po.page.url()).toEqual("file:///settings");
 });
